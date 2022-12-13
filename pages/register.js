83f655d9722da28768d7820ad36f5dd739fb7e06
Rahul Gaur <rahulgaur10441@gmail.com>
2022-12-13 17:52:42 +0530

    ragister page

--- pages/register.js
+++ pages/register.js
@@ -3,9 +3,9 @@ import React from "react";
 const Register = () => {
     return (
         <div>
-            <section className="vh-100" >
-                <div className="container h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
+            <section >
+                <div className="container">
+                    <div className="row d-flex justify-content-center align-items-center">
                         <div className="col-lg-12 col-xl-11">
                             <div className="card text-black">
                                 <div className="card-body p-md-5">
@@ -17,7 +17,7 @@ const Register = () => {
                                             <form className="mx-1 mx-md-4">
 
                                                 <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+
                                                     <div className="form-outline flex-fill mb-0">
                                                         <input type="text" id="form3Example1c" className="form-control" />
                                                         <label className="form-label" for="form3Example1c">Your Name</label>
@@ -25,7 +25,7 @@ const Register = () => {
                                                 </div>
 
                                                 <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+
                                                     <div className="form-outline flex-fill mb-0">
                                                         <input type="email" id="form3Example3c" className="form-control" />
                                                         <label className="form-label" for="form3Example3c">Your Email</label>
@@ -33,7 +33,7 @@ const Register = () => {
                                                 </div>
 
                                                 <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+
                                                     <div className="form-outline flex-fill mb-0">
                                                         <input type="password" id="form3Example4c" className="form-control" />
                                                         <label className="form-label" for="form3Example4c">Password</label>
@@ -41,7 +41,7 @@ const Register = () => {
                                                 </div>
 
                                                 <div className="d-flex flex-row align-items-center mb-4">
-                                                    <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+
                                                     <div className="form-outline flex-fill mb-0">
                                                         <input type="password" id="form3Example4cd" className="form-control" />
                                                         <label className="form-label" for="form3Example4cd">Repeat your password</label>
