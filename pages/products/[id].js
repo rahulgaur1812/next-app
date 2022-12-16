c7930fef5c51566857c7e528f040cfb7ceef031b
Rahul Gaur <rahulgaur10441@gmail.com>
2022-12-16 17:38:42 +0530

    product detail page

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from 'next/router';
-
+import style from '../../styles/Product.module.scss';
 export const getServerSideProps = async (context) => {
     const id = context.params.id;
     console.log("id", id);
@@ -14,10 +14,29 @@ export const getServerSideProps = async (context) => {
 const ProductId = (props) => {
     console.log("pp", props);
     return (
-        <div className="row">
-            this is product page -{props.productData.title}
+        // <div className="row">
+        //     this is product page -{props.productData.title}
+        //     <div className="col-lg-4"><img className={style.productImage} src={props.productData.image} /></div>
+        //     <div className="col-lg-8">
+        //         <div className="row">
+        //             <h2>{props.productData.title}</h2>
+        //             <div className="h6">{props.productData.description}</div>
+        //         </div>
+        //     </div>
 
-        </div >
+        // </div >
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{props.productData.title}</h5>
+                <p className="card-text">{props.productData.description}</p>
+                <p className="card-text"><small className="text-muted">{props.productData.category}</small></p>
+                <p className="card-text"><small className="text-muted">Price: Rs {props.productData.price}</small></p>
+            </div>
+            <div className={style.cardImage}>
+                <img src={props.productData.image} className={`${style.productImage} card-img-bottom`} alt="..." />
+            </div>
+
+        </div>
     )
 }
 // make api call using id
